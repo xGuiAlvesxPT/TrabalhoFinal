cba212735f7286712a82b863d6d13482e2b7c948
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-06-15 21:32:08 +0100

    Atualização do nome das labels dos fragmentos !

--- app/src/main/java/pt/ipg/trabalhofinal/FragmentPaginaGestao.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentPaginaGestao.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import pt.ipg.trabalhofinal.databinding.FragmentSecondBinding
+import pt.ipg.trabalhofinal.databinding.FragmentPaginagestaoBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class FragmentPaginaGestao : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentPaginagestaoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentPaginagestaoBinding.inflate(inflater, container, false)
         return binding.root
 
     }
--- app/src/main/java/pt/ipg/trabalhofinal/FragmentPaginaInicial.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentPaginaInicial.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import pt.ipg.trabalhofinal.databinding.FragmentFirstBinding
+import pt.ipg.trabalhofinal.databinding.FragmentPaginainicialBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FragmentPaginaInicial : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentPaginainicialBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentPaginainicialBinding.inflate(inflater, container, false)
         return binding.root
 
     }
