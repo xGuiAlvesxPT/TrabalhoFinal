9cd3167c77935abf1e66be85784777609d4f9707
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-06-17 17:08:55 +0100

    Inserção direta de dados na tabela Sexo!

--- app/src/main/java/pt/ipg/trabalhofinal/TabelaSexo.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/TabelaSexo.kt
@@ -8,6 +8,8 @@ class TabelaSexo ( db: SQLiteDatabase): TabelaBD(db, TabelaPlataformas.NOME) {
     override fun cria (){
 
         db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $NOME_SEXO TEXT NOT NULL) ")
+        db.execSQL("INSERT INTO $NOME ($NOME_SEXO) VALUES ('Masculino')")
+        db.execSQL("INSERT INTO $NOME ($NOME_SEXO) VALUES ('Feminino')")
     }
 
     companion object{
