e0bf8f316ea0d3474a49fbfdac918227569bf46a
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-07-06 19:22:11 +0100

    Adicionado um argumento (cliente) ao fragmento Eliminar cliente

--- app/src/main/java/pt/ipg/trabalhofinal/Sexo.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/Sexo.kt
@@ -3,11 +3,13 @@ package pt.ipg.trabalhofinal
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
+import java.io.Serializable
 
 data class Sexo(
     var nomeSexo: String,
-    var id: Long = -1
-) {
+    var id: Long = -1,
+
+) : Serializable {
 
     fun toContentValues(): ContentValues {
 
