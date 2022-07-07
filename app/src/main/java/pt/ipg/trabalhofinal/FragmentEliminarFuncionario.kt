57b90b660e15995fd412dd1b747c189eab53e72f
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-07-07 23:04:25 +0100

    Implementadas opções cancelar e eliminar no fragmento eliminar funcionario(eliminar funcionario concluído)

--- app/src/main/java/pt/ipg/trabalhofinal/FragmentEliminarFuncionario.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentEliminarFuncionario.kt
@@ -1,11 +1,17 @@
 package pt.ipg.trabalhofinal
 
+import android.content.DialogInterface
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import pt.ipg.trabalhofinal.databinding.FragmentEliminarFuncionarioBinding
 
 
@@ -49,12 +55,42 @@ class FragmentEliminarFuncionario : Fragment() {
     fun processaOpcaoMenu(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_eliminar -> {
+                confirmaEliminaFuncionario()
                 true
             }
             R.id.action_cancelar -> {
+                voltaListaFuncionarios()
                 true
             }
             else -> false
         }
+    }
+
+    private fun confirmaEliminaFuncionario() {
+        val alert = AlertDialog.Builder(requireContext())
+        alert.setTitle("Apagar Funcionario")
+        alert.setMessage("Deseja apagar o funcionario?")
+        alert.setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialog, which ->  })
+        alert.setPositiveButton(R.string.eliminar, DialogInterface.OnClickListener { dialog, which -> eliminaFuncionario() })
+        alert.show()
+    }
+
+    private fun eliminaFuncionario() {
+        val enderecoFuncionarioApagar = Uri.withAppendedPath(ContentProviderLojaJogos.ENDERECO_FUNCIONARIOS, "${funcionario.id}")
+
+        val registosEliminados = requireActivity().contentResolver.delete(enderecoFuncionarioApagar, null, null)
+
+        if (registosEliminados == 1) {
+            Toast.makeText(requireContext(), "Funcionario eliminado com sucesso", Toast.LENGTH_LONG).show()
+            voltaListaFuncionarios()
+        } else {
+            Snackbar.make(binding.textViewNomeFuncionarioEli, "Erro ao eliminar funcionario", Snackbar.LENGTH_INDEFINITE).show()
+        }
+    }
+
+    private fun voltaListaFuncionarios() {
+        val acao =
+            FragmentEliminarFuncionarioDirections.actionFragmentEliminarFuncionarioToFragmentverfuncionarios()
+        findNavController().navigate(acao)
     }
 }
