058e35e6ef36a99eae2ceee044707e75407e3631
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-07-07 20:54:37 +0100

    Adicionado menu de edição para inserir/alterar (funcionarios)

--- app/src/main/java/pt/ipg/trabalhofinal/FragmentEditarFuncionarios.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentEditarFuncionarios.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -43,11 +44,16 @@ class FragmentEditarFuncionarios : Fragment() {
 
         val activity = activity as MainActivity
         activity.fragment = this
-        //activity.idMenuAtual = R.menu.
+        activity.idMenuAtual = R.menu.menu_editar
     }
 
     fun processaOpcaoMenu(item: MenuItem): Boolean {
         return when (item.itemId) {
+            R.id.action_eliminar -> true
+            R.id.action_cancelar -> {
+                findNavController().navigate(R.id.action_fragmentEditarFuncionarios_to_fragmentverfuncionarios)
+                true
+            }
             else -> false
         }
     }
