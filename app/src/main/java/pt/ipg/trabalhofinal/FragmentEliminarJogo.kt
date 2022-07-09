e2779d63cb3716adf0e3ae4c5715c59a30d325b5
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-07-09 18:35:19 +0100

    Implementadas opções cancelar e eliminar no fragmento eliminar jogo (eliminar jogo concluído)

--- app/src/main/java/pt/ipg/trabalhofinal/FragmentEliminarJogo.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentEliminarJogo.kt
@@ -1,11 +1,17 @@
 package pt.ipg.trabalhofinal
 
+import android.content.DialogInterface
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import pt.ipg.trabalhofinal.databinding.FragmentEliminarJogoBinding
 
 
@@ -51,12 +57,41 @@ class FragmentEliminarJogo : Fragment() {
     fun processaOpcaoMenu(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_eliminar -> {
+                confirmaEliminaJogo()
                 true
             }
             R.id.action_cancelar -> {
+                voltaListaJogos()
                 true
             }
             else -> false
         }
+    }
+
+    private fun confirmaEliminaJogo() {
+        val alert = AlertDialog.Builder(requireContext())
+        alert.setTitle("Apagar jogo")
+        alert.setMessage("Deseja apagar o jogo?")
+        alert.setNegativeButton(android.R.string.cancel, DialogInterface.OnClickListener { dialog, which ->  })
+        alert.setPositiveButton(R.string.eliminar, DialogInterface.OnClickListener { dialog, which -> eliminaJogo() })
+        alert.show()
+    }
+
+    private fun eliminaJogo() {
+        val enderecoJogoApagar = Uri.withAppendedPath(ContentProviderLojaJogos.ENDERECO_JOGOS, "${jogo.id}")
+
+        val registosEliminados = requireActivity().contentResolver.delete(enderecoJogoApagar, null, null)
+
+        if (registosEliminados == 1) {
+            Toast.makeText(requireContext(), "Jogo eliminado com sucesso", Toast.LENGTH_LONG).show()
+            voltaListaJogos()
+        } else {
+            Snackbar.make(binding.textViewNomeJogoEli, "Erro ao eliminar jogo", Snackbar.LENGTH_INDEFINITE).show()
+        }
+    }
+
+    private fun voltaListaJogos() {
+        val acao = FragmentEliminarJogoDirections.actionFragmentEliminarJogoToFragmentVerJogos()
+        findNavController().navigate(acao)
     }
 }
