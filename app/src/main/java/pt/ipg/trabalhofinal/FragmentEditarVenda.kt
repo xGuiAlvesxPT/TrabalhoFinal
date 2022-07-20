7c598fc4853c28aefbd2f15ea2b5d41280b70ea1
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-07-20 21:50:31 +0100

    Fragmento de inserir venda completo

--- app/src/main/java/pt/ipg/trabalhofinal/FragmentEditarVenda.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentEditarVenda.kt
@@ -1,6 +1,7 @@
 package pt.ipg.trabalhofinal
 
 import android.database.Cursor
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +9,16 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SimpleCursorAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import pt.ipg.trabalhofinal.databinding.FragmentEditarJogoBinding
 import pt.ipg.trabalhofinal.databinding.FragmentEditarVendaBinding
+import java.util.*
 
 
 /**
@@ -25,6 +30,9 @@ class FragmentEditarVenda : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     private var loader: CursorLoader? = null
     private var _binding: FragmentEditarVendaBinding? = null
+    private var venda: Venda? = null
+    var dataVenda: Long = 0L
+    var data: String = ""
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -42,7 +50,7 @@ class FragmentEditarVenda : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val activity = activity as MainActivity
+        val activity = requireActivity() as MainActivity
         activity.fragment = this
         activity.idMenuAtual = R.menu.menu_editar
 
@@ -50,14 +58,57 @@ class FragmentEditarVenda : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
         LoaderManager.getInstance(this).initLoader(ID_LOADER_CLIENTE, null, this)
         LoaderManager.getInstance(this).initLoader(ID_LOADER_FUNCIONARIO, null, this)
 
+        if (arguments != null) {
+          //  venda = FragmentEditarVendaArgs.fromBundle(arguments!!).venda
+            if (venda != null) {
+                binding.textViewNovaVenda.setText("ATUALIZAR JOGO")
+                binding.EditTextQuantidade.setText(venda!!.quantidade.toString())
+                binding.EditTextPrecoVenda.setText(venda!!.precoVenda.toString())
+
+                val getData = venda!!.data_de_venda
+                val dateFormat = SimpleDateFormat("dd-MM-yyy")
+                val dataFormatada = dateFormat.format(getData)
+                val dateSplit = dataFormatada.split("-")
+                val ano = dateSplit[2]
+                val mes = dateSplit[1]
+                val dia = dateSplit[0]
+                val currentDate = Calendar.getInstance()
+                binding.datePickerDataVenda.init(
+                    ano.toInt(),
+                    mes.toInt() - 1,
+                    dia.toInt()
+                ) { view, ano, mes, dia ->
+                    currentDate.set(ano, mes, dia)
+                    dataVenda = currentDate.timeInMillis
+                }
+            } else {
+                val picker = binding.datePickerDataVenda
+                val currentDate = Calendar.getInstance()
+                picker.init(
+                    currentDate.get(Calendar.YEAR),
+                    currentDate.get(Calendar.MONTH),
+                    currentDate.get(Calendar.DAY_OF_MONTH)
+
+                ) { view, ano, mes, dia ->
+                    //val mes = mes
+                    currentDate.set(ano, mes, dia)
+                    dataVenda = currentDate.timeInMillis
+                }
+            }
+
+        }
+
     }
 
 
     fun processaOpcaoMenu(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_eliminar -> true
+            R.id.action_eliminar -> {
+                guardar()
+                true
+            }
             R.id.action_cancelar -> {
-                findNavController().navigate(R.id.action_fragmentEditarVenda_to_fragmentVerVendas)
+                voltaListaVendas()
                 true
             }
             else -> false
@@ -69,6 +120,67 @@ class FragmentEditarVenda : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
         const val ID_LOADER_CLIENTE = 1
         const val ID_LOADER_FUNCIONARIO = 2
     }
+
+    private fun voltaListaVendas() {
+        findNavController().navigate(R.id.action_fragmentEditarVenda_to_fragmentVerVendas)
+    }
+
+    private fun guardar() {
+        val quantidade = binding.EditTextQuantidade.text.toString()
+        if (quantidade.isBlank()) {
+            binding.EditTextQuantidade.error = getString(R.string.quantidadeObrigatoria)
+            binding.EditTextQuantidade.requestFocus()
+            return
+        }
+
+        val preco = binding.EditTextPrecoVenda.text.toString()
+        if (preco.isBlank()) {
+            binding.EditTextPrecoVenda.error = getString(R.string.precoObrigatorioo)
+            binding.EditTextPrecoVenda.requestFocus()
+            return
+        }
+
+        val idFuncionarios = binding.spinnerVendaFuncionario.selectedItemId
+        if (idFuncionarios == Spinner.INVALID_ROW_ID) {
+            binding.textViewfun.error = getString(R.string.funObrigatorio)
+            binding.spinnerVendaFuncionario.requestFocus()
+            return
+        }
+
+        val idCliente = binding.spinnerVendaCliente.selectedItemId
+        if (idCliente == Spinner.INVALID_ROW_ID) {
+            binding.textViewVendaClientee.error = getString(R.string.clienteObrigatorio)
+            binding.spinnerVendaCliente.requestFocus()
+            return
+        }
+
+        val idJogo= binding.spinnerNomeJogo.selectedItemId
+        if (idJogo == Spinner.INVALID_ROW_ID) {
+            binding.textViewJogo.error = getString(R.string.jogoObrigatorio)
+            binding.spinnerNomeJogo.requestFocus()
+            return
+        }
+
+        val venda = Venda(
+            quantidade.toLong(),preco.toDouble(), dataVenda,Cliente("","","",0,Sexo("",idCliente))
+            , Funcionario("",",","",0,idFuncionarios),Sexo(""),Jogo("",0.0,"","",0,
+                Plataforma(""),idJogo
+            ), Plataforma(""))
+
+
+        val endereco = requireActivity().contentResolver.insert(
+            ContentProviderLojaJogos.ENDERECO_VENDAS,
+            venda.toContentValues()
+        )
+
+        if (endereco != null) {
+            Toast.makeText(requireContext(), "Venda inserida com sucesso", Toast.LENGTH_LONG).show()
+            voltaListaVendas()
+        } else {
+            Snackbar.make(binding.EditTextPrecoVenda, "Erro ao inserir venda", Snackbar.LENGTH_INDEFINITE).show()
+        }
+    }
+
 
 
     /**
