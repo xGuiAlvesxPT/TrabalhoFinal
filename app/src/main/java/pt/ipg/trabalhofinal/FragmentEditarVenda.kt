de5d626aaee43b86c6389596adbbcd3192145582
xGuiAlvesxPT <101353229+xGuiAlvesxPT@users.noreply.github.com>
2022-07-20 20:57:47 +0100

    Ler dados dos funcionarios, clientes e jogos no spinner!

--- app/src/main/java/pt/ipg/trabalhofinal/FragmentEditarVenda.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/FragmentEditarVenda.kt
@@ -1,11 +1,15 @@
 package pt.ipg.trabalhofinal
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,10 +22,11 @@ private const val ARG_PARAM2 = "param2"
  * Use the [FragmentEditarVenda.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentEditarVenda : Fragment() {
+class FragmentEditarVenda : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var loader: CursorLoader? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,7 +50,13 @@ class FragmentEditarVenda : Fragment() {
         val activity = activity as MainActivity
         activity.fragment = this
         activity.idMenuAtual = R.menu.menu_editar
+
+        LoaderManager.getInstance(this).initLoader(ID_LOADER_JOGO, null, this)
+        LoaderManager.getInstance(this).initLoader(ID_LOADER_CLIENTE, null, this)
+        LoaderManager.getInstance(this).initLoader(ID_LOADER_FUNCIONARIO, null, this)
+
     }
+
 
     fun processaOpcaoMenu(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -58,5 +69,115 @@ class FragmentEditarVenda : Fragment() {
         }
     }
 
+    companion object {
+        const val ID_LOADER_JOGO = 0
+        const val ID_LOADER_CLIENTE = 1
+        const val ID_LOADER_FUNCIONARIO = 2
+    }
 
+
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param id The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        if (id == ID_LOADER_JOGO) {
+        loader =    CursorLoader(
+                requireContext(),
+                ContentProviderLojaJogos.ENDERECO_JOGOS,
+                TabelaJogos.TODAS_COLUNAS,
+                null,
+                null,
+                TabelaJogos.NOME_JOGO
+            )
+        }else if (id == ID_LOADER_CLIENTE) {
+         loader =    CursorLoader(
+                requireContext(),
+                ContentProviderLojaJogos.ENDERECO_CLIENTES,
+                TabelaClientes.TODAS_COLUNAS,
+                null,
+                null,
+                TabelaClientes.NOME_CLIENTE
+            )
+        }else if (id == ID_LOADER_FUNCIONARIO) {
+        loader = CursorLoader(
+            requireContext(),
+            ContentProviderLojaJogos.ENDERECO_FUNCIONARIOS,
+            TabelaFuncionarios.TODAS_COLUNAS,
+            null,
+            null,
+            TabelaFuncionarios.NOME_FUNCIONARIO
+        )
+    }
+        return loader as CursorLoader
+
+    }
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is *not* allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See [ FragmentManager.openTransaction()][androidx.fragment.app.FragmentManager.beginTransaction] for further discussion on this.
+     *
+     *
+     * This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     *
+     *
+     *  *
+     *
+     *The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a [android.database.Cursor]
+     * and you place it in a [android.widget.CursorAdapter], use
+     * the [android.widget.CursorAdapter.CursorAdapter] constructor *without* passing
+     * in either [android.widget.CursorAdapter.FLAG_AUTO_REQUERY]
+     * or [android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER]
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     *  *  The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a [android.database.Cursor] from a [android.content.CursorLoader],
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * [android.widget.CursorAdapter], you should use the
+     * [android.widget.CursorAdapter.swapCursor]
+     * method so that the old Cursor is not closed.
+     *
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
+    }
 }
+
+
